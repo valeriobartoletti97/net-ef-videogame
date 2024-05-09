2cdfe24b6dde203ba5a896d28a7926ab5365fe91
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-09 09:33:51 +0200

    fix bug

--- VideogameContext.cs
+++ VideogameContext.cs
@@ -10,7 +10,7 @@ namespace net_ef_videogame
     public class VideogameContext : DbContext
     {
         public DbSet<Videogame> Videogames { get; set; }
-        public DbSet<SoftwareHouse> Courses { get; set; }
+        public DbSet<SoftwareHouse> SoftwareHouses { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
